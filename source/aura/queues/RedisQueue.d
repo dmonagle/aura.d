3ad69800801f16f20a710ccef7bc9446b22ea6de
David Monagle <david.monagle@intrica.com.au>
2014-12-22 10:45:22 +1100

    Added new RedisProcessingQueue

--- source/aura/queues/RedisQueue.d
+++ source/aura/queues/RedisQueue.d
@@ -5,36 +5,41 @@ import vibe.db.redis.redis;
 
 /// A Redis based FIFO queue that will only accept unique values
 struct RedisQueue {
-	@property string setName() const {
-		return _redisKey ~ "/queueSet";
+	@property string listKey() const {
+		return _redisKey ~ "/list";
+	}
+
+	@property string listLockKey() const {
+		return _redisKey ~ "/lock";
 	}
 	
-	@property string listName() const {
-		return _redisKey ~ "/queueList";
+	@property string processingLockKey() const {
+		return _redisKey ~ "/plock";
 	}
 
 	@property long length() {
-		return _database.llen(listName);
+		return _database.llen(listKey);
 	}
 	
 	bool push(T)(T id) {
-		if (_database.sadd(setName, id)) {
-			_database.rpush(listName, id);
+		if (_database.sadd(listLockKey, id)) {
+			_database.rpush(listKey, id);
 			return true;
 		}
 		return false;
 	}
 
 	Nullable!string pop() {
-		Nullable!string returnString = _database.lpop!(Nullable!string)(listName);
+		Nullable!string returnString = _database.lpop!(Nullable!string)(listKey);
 		if (!returnString.isNull) {
-			_database.srem(setName, returnString);
+			_database.srem(listLockKey, returnString);
 		}
 		return returnString;
 	}
 
-	void destroy() {
-		_database.del(listName, setName);
+	/// Clears the queue by removing the keys from the database
+	void clear() {
+		_database.del(listKey, listLockKey);
 	}
 
 	this(string queueName, RedisDatabase database) {
@@ -52,7 +57,7 @@ unittest {
 	auto db = redisClient.getDatabase(0);
 
 	auto q = RedisQueue("unittest/testQueue", db);
-	q.destroy;
+	q.clear;
 	assert(q.length == 0);
 	assert(q.pop.isNull);
 	assert(q.push("one"));
@@ -66,6 +71,227 @@ unittest {
 	assert(q.pop() == "two");
 	assert(q.length == 0);
 	assert(q.pop.isNull);
-	q.destroy;
+	q.clear;
 }
 
+import aura.queues.redis_locks;
+import vibe.core.core;
+import std.datetime;
+import std.conv;
+
+struct RedisProcessingQueue {
+	this(RedisDatabase database, string queueName, Duration queueDuration = 24.hours, string processingLockKey = "") {
+		_redisKey = queueName;
+		_pLockKey = processingLockKey.length ? processingLockKey : _redisKey ~ "/plock";
+		_database = database;
+		_requeueTimeout = 10.seconds;
+		_queueDuration = queueDuration;
+		_processingLockDuration = 2.minutes;
+	}
+
+	@property string name() const {
+		return _redisKey;
+	}
+
+	@property string listKey() const {
+		return _redisKey ~ "/list";
+	}
+	
+	@property string listLockKey(T)(T id) const {
+		return _redisKey ~ "/llock/" ~ id.to!string;
+	}
+	
+	@property string processingLockKey(T)(T id) const {
+		return _pLockKey ~ "/" ~ id.to!string;
+	}
+	
+	@property long length() {
+		return _database.llen(listKey);
+	}
+
+	// Clears the queue by removing the queue list and all of the list locks. This does not clear the processing locks
+	void clear() {
+		_database.del(listKey);
+		auto keys = _database.keys(_redisKey ~ "/llock/*");
+		foreach(key; keys) {
+			_database.del(key);
+		}
+	}
+
+	void forcePush(string id) {
+		auto listLock = RedisLock(_database, listLockKey(id), _queueDuration);
+		listLock.forceUnlock;
+		push(id);
+	}
+
+	bool push(string id) {
+		auto listLock = RedisLock(_database, listLockKey(id), _queueDuration);
+
+		if (listLock.lock()) {
+			_database.rpush(listKey, id);
+			return true;
+		}
+		return false;
+	}
+	
+	bool process(scope void delegate(string) action = null) {
+		Nullable!string returnString = _database.lpop!(Nullable!string)(listKey); 
+		if (returnString.isNull) return false;
+		auto id = returnString.get;
+		auto lLock = RedisLock(_database, listLockKey(id));
+		lLock.touch(_requeueTimeout + 2.seconds); // Now if anything happens to prevent requeuing, the list lock will expire by itself.
+
+
+		auto pLock = RedisScopedLock(_database, processingLockKey(id), _processingLockDuration); // Attempt to get a processing lock
+		try {
+			if (pLock.lock) {
+				lLock.forceUnlock; // Force unlock of list lock. As this struct didn't create it, force is required
+				action(id);
+
+				return true; // Processing Lock will be freed automatically
+			}
+			else {
+				// We add the id back to the queue after the requeue timeout
+				setTimer(_requeueTimeout, () {
+					_database.rpush(listKey, id);
+					lLock.touch(); // Set the listLock back to the default value
+				});
+				return false;
+			}
+		}
+		catch (Exception e) {
+			// This error perhaps shouldn't be caught. It normally means a redis issue
+		}
+
+		return false;
+	}
+
+private:
+	RedisDatabase _database;
+	string _redisKey;
+	string _pLockKey;
+	Duration _queueDuration;
+	Duration _requeueTimeout;
+	Duration _processingLockDuration;
+
+	Task[] _tasks;
+
+}
+
+unittest {
+	auto redisClient = new RedisClient();
+	auto db = redisClient.getDatabase(0);
+	
+	auto queue = RedisProcessingQueue(db, "unittest/testQueue");
+	queue.clear();
+
+	assert(queue.push("one"));
+	assert(!queue.push("one"), "Managed to add a second copy of an id already in the queue");
+	queue.process((id) {
+	});
+}
+
+import vibe.core.log;
+import colorize;
+
+struct RedisWorkerQueue {
+	this(RedisDatabase database, string queueName, Duration queueDuration = 24.hours, string processingLockKey = "") {
+		_queue = RedisProcessingQueue(database, queueName, queueDuration, processingLockKey);
+	}
+
+	void worker() {
+		while (_processing) {
+			if (!_queue.process((id) {
+				_workersBusy++;
+				logInfo("Worker: %s Queue size: %s Processing: %s".color(fg.light_magenta), queue.name, _queue.length, id);
+				
+				try {
+					_action(id);
+				}
+				catch (Exception e) {
+					logError("While processing id: %s for queue %s: %s".color(fg.light_red), id, queue.name, e.msg);
+				}
+				_workersBusy--;
+				logInfo("Queue item finished");
+			})) sleep(200.msecs);
+		}
+		logInfo("Worker finishing".color(fg.magenta));
+	}
+	
+	void runWorkers(scope void delegate(string) action, ulong workerCount) {
+		if (!_processing) {
+			_processing = true;
+			_action = action;
+			foreach (i; 0 .. workerCount) _tasks ~= runTask(&worker);
+		}
+	}
+
+	bool push(string id) {
+		return queue.push(id);
+	}
+
+	void join() {
+		_processing = false;
+		foreach (t; _tasks) t.join();
+	}
+
+	@property ref RedisProcessingQueue queue() {
+		return _queue;
+	}
+
+	// Clears the underlying RedisProcessingQueue
+	void clear() {
+		_queue.clear;
+	}
+
+private:
+	bool _processing;
+	//ulong _workerCount;
+	ulong _workersBusy;
+	
+	Task[] _tasks;
+	RedisProcessingQueue _queue;
+	void delegate(string) _action;
+}
+
+// This unit test is commented out by default as it takes significant time to run. Should probably be run based on a version
+/*
+unittest {
+	import std.stdio;
+	import colorize;
+
+	writeln("Starting test".color(fg.light_blue));
+
+	auto redisClient = new RedisClient();
+	auto db = redisClient.getDatabase(0);
+
+	auto queue = RedisWorkerQueue(db, "unittest/testQueue");
+	queue.clear;
+	queue.runWorkers((id) {
+		writeln("Processing ID: " ~ id);
+		sleep(20.seconds);
+	}, 2);
+
+	writeln("Queue has been created. Pushing in 3 seconds".color(fg.light_green));
+	sleep(3.seconds);
+	assert(queue.push("one"));
+	sleep(2.seconds);
+	assert(queue.push("two"));
+	sleep(2.seconds);
+	// This should add but should fail to process immediately as it should still be processing
+	assert(queue.push("two"));
+	// The following should queue up
+	assert(queue.push("three"));
+	assert(queue.push("four"));
+	// These should refuse to add as they are already in the queue
+	assert(!queue.push("three"));
+	assert(!queue.push("four"));
+
+	writeln("Finished pushing".color(fg.cyan));
+	sleep(240.seconds);
+	queue.join();
+
+	writeln("Finishing test".color(fg.light_blue));
+
+}
+*/
---
+++ source/aura/queues/redis_locks.d
@@ -0,0 +1,175 @@
+﻿module aura.queues.redis_locks;
+
+import vibe.db.redis.redis;
+import std.datetime;
+import std.conv;
+import std.random;
+
+struct RedisLockBase(bool scopedUnlock = false) {
+	this(RedisDatabase database, string redisKey, Duration expiryTime = 2.seconds) {
+		_redisKey = redisKey;
+		_database = database;
+		_expiryTime = expiryTime;
+	}
+	
+	@property int id() const {
+		return _lockId;
+	}
+	
+	bool locked() {
+		return _database.exists(_redisKey);
+	}
+	
+	bool lock() {
+		return tryLock;
+	}
+	
+	void lock(scope void delegate() success = null, scope void delegate() fail = null) {
+		if (tryLock) {
+			scope(exit) {
+				forceUnlock;
+			}
+			if (success != null) success();
+		}
+		else {
+			if (fail != null) fail();
+		}
+		
+	}
+	
+	bool unlock() {
+		if (mine) {
+			forceUnlock;
+			return true;
+		}
+		return false;
+	}
+
+	void forceUnlock() {
+		_database.del(_redisKey);
+	}
+	
+	bool touch(Duration expiryTime) {
+		if (mine) {
+			if (expiryTime == Duration.zero) 
+				_database.persist(_redisKey);
+			else 
+				_database.expire(_redisKey, _expiryTime);
+			return true;
+		}
+		return false;
+	}
+	
+	bool touch() {
+		return touch(_expiryTime);
+	}
+	
+	int activeLockId() {
+		auto result = _database.get(_redisKey);
+		if (!result.length) return 0;
+		return result.to!int;
+	}
+	
+	// Returns true if the lock is held by this struct
+	bool mine() {
+		if (_lockId && (activeLockId == _lockId)) return true;
+		return false;
+	}
+
+	static if (scopedUnlock) {
+		~this() {
+			import std.stdio;
+			// Only remove the lock if it was locked by this instance
+			unlock();
+		}
+	}
+
+	
+private:
+	RedisDatabase _database;
+	string _redisKey;
+	int _lockId;
+	Duration _expiryTime;
+	
+	bool tryLock() {
+		if (!_lockId) _lockId = uniform(1, int.max);
+		if (_database.setNX(_redisKey, _lockId.to!string, _expiryTime)) {
+			return true;
+		}
+		return false;
+	}
+
+
+}
+
+alias RedisLock = RedisLockBase!(false);
+
+unittest {
+	auto redisClient = new RedisClient();
+	auto db = redisClient.getDatabase(0);
+	
+	if (true) {
+		auto l = RedisLock(db, "unittest/lockTest1");
+		assert(l.lock());
+		assert(l.locked);
+		assert(l.mine); 
+	}
+	
+	// Previous lock should be valid from outside scope
+	auto l = RedisLock(db, "unittest/lockTest1");
+	assert(l.locked);
+	assert(!l.lock);
+	assert(!l.mine);
+	assert(!l.unlock); // Can't call unlock as this struct didn't create the lock
+	assert(l.locked);
+	l.forceUnlock; // It can be forced though
+	assert(!l.locked);
+}
+
+
+alias RedisScopedLock = RedisLockBase!(true);
+
+unittest {
+	auto redisClient = new RedisClient();
+	auto db = redisClient.getDatabase(0);
+
+	if (true) {
+		auto l = RedisScopedLock(db, "unittest/lockTest1");
+		assert(l.lock());
+		assert(l.locked);
+		assert(l.mine); 
+
+		auto l2 = RedisScopedLock(db, "unittest/lockTest1");
+		assert(!l2.lock()); // Lock should fail as it uses the same key as the previous lock
+		assert(l2.locked); // It should still be locked however
+		assert(!l2.mine); // But the lock doesn't belong to this struct
+	}
+
+	// Previous locks should be released after they go out of scope
+	auto l = RedisScopedLock(db, "unittest/lockTest1");
+	assert(!l.locked);
+	assert(l.lock);
+}
+
+// Test the lambda version
+unittest {
+	auto redisClient = new RedisClient();
+	auto db = redisClient.getDatabase(0);
+
+	auto l = RedisScopedLock(db, "unittest/lockTest");
+
+	bool firstLock;
+	bool secondLockFail;
+
+	l.lock(() {
+		firstLock = true;
+		l.lock(() {
+			secondLockFail = false;
+		},
+		() {
+			secondLockFail = true;
+		});
+	});
+	assert(firstLock);
+	assert(secondLockFail);
+}
