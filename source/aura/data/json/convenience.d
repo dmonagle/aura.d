621c764b1b5f85a8a27e9c4eb178e848e13e59a8
David Monagle <david.monagle@intrica.com.au>
2015-01-09 16:38:09 +1100

    Added json.convenience

---
+++ source/aura/data/json/convenience.d
@@ -0,0 +1,42 @@
+﻿module aura.data.json.convenience;
+
+public import vibe.data.json;
+
+
+bool isObject(const ref Json json) {
+	return json.type == Json.Type.object;
+}
+
+bool isArray(const ref Json json) {
+	return json.type == Json.Type.Array;
+}
+
+bool isString(const ref Json json) {
+	return json.type == Json.Type.string;
+}
+
+bool isBool(const ref Json json) {
+	return json.type == Json.Type.bool_;
+}
+
+bool isInt(const ref Json json) {
+	return json.type == Json.Type.int_;
+}
+
+bool isFloat(const ref Json json) {
+	return json.type == Json.Type.float_;
+}
+
+bool isNumber(const ref Json json) {
+	return (json.isInt || json.isFloat);
+}
+
+
+bool isUndefined(const ref Json json) {
+	return json.type == Json.Type.undefined;
+}
+
+bool isNull(const ref Json json) {
+	return json.type == Json.Type.null_;
+}
+
--- source/aura/data/json/package.d
+++ source/aura/data/json/package.d
@@ -7,4 +7,5 @@ public import aura.data.json.dup;
 public import aura.data.json.filter;
 public import aura.data.json.merge;
 public import aura.data.json.builder;
+public import aura.data.json.convenience;
 
