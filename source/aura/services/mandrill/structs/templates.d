ea48a23a649b05b94d9ddc8a5c698fb6d3807c77
David Monagle <david.monagle@intrica.com.au>
2015-02-07 20:42:37 +1100

    Removed individual mandrill struct files

--- source/aura/services/mandrill/structs/templates.d
+++
@@ -1,9 +0,0 @@
-﻿module aura.services.mandrill.structs.templates;
-
-/// the injection of a single piece of content into a single editable region
-struct TemplateContent {
-	/// the name of the mc:edit editable region to inject into
-	string name;
-	/// the content to inject
-	string content;
-}
