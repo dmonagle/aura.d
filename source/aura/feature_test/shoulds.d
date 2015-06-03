36ef245917e8c203894e037e356bad0862d3f0c7
David Monagle <david.monagle@intrica.com.au>
2015-06-03 20:35:06 +1000

    Made the shoulds more elegant

--- source/aura/feature_test/shoulds.d
+++ source/aura/feature_test/shoulds.d
@@ -21,10 +21,35 @@ debug (featureTest) {
 		throw new FeatureTestException(format("REST Exception did not occur when expecting %s(%s)", status.to!string, status), file, line);
 	}
 
+	template should(alias operation, string description) {
+		bool should(E)(lazy E expression, string name="Value", string file = __FILE__, typeof(__LINE__) line = __LINE__) {
+			auto eValue = expression;
+			if (operation(eValue)) return true;
+			throw new FeatureTestException(format("%s should %s, but it was %s", name, description, eValue), file, line);
+		}
+	}
+
+	template shouldValue(alias operation, string description) {
+		bool shouldValue(E, V)(lazy E expression, V value, string name="Value", string file = __FILE__, typeof(__LINE__) line = __LINE__) {
+			auto eValue = expression;
+			if (operation(eValue, value)) return true;
+			throw new FeatureTestException(format("%s should %s %s, but was actually %s", name, description, value, eValue), file, line);
+		}
+	}
+
+	alias shouldBeTrue = should!((e) => e ? true : false, "be true");
+	alias shouldBeFalse = should!((e) => e ? false : true, "be false");
+	alias shouldEqual = shouldValue!((e, v) => e == v, "equal");
+	alias shouldBeGreaterThan = shouldValue!((e, v) => e > v, "be greater than");
+	alias shouldNotBeEmpty = should!((e) => e.length ? true : false, "not be empty");
+
 	import aura.data.json;
+	/// Asserts the the given expression is of a specific Json type
 	bool shouldBeJson(string type)(Json object, string file = __FILE__, typeof(__LINE__) line = __LINE__) {
 		bool function(ref const Json) check = mixin("&is" ~ type);
 		if (check(object)) return true;
 		throw new FeatureTestException(format("Should be JSON type %s but got %s", type, object.type), file, line);
 	}
 }
+
+
