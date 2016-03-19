7186c4e5c3a6da9b78d77a64cd2b0daee24a0acb
David Monagle <david.monagle@intrica.com.au>
2016-03-19 14:06:05 +1100

    Changed graph belongs to relationships to use resolver

--- components/graph/source/aura/graph/relationships.d
+++ components/graph/source/aura/graph/relationships.d
@@ -4,6 +4,39 @@ import aura.graph;
 import transforms;
 import inflections.en;
 
+
+string defineGraphBelongsToProperty(P : GraphModelInterface, M : GraphModelInterface, string propertyName, string key, string foreignKey)() {
+	import std.format;
+	
+	static if (!propertyName.length) 
+		enum _propertyName = M.stringof.camelCaseLower;
+	else
+		enum _propertyName = propertyName;
+	
+	static if (!key.length)
+		enum _key = _propertyName ~ "Id";
+	else
+		enum _key = key;
+        
+    enum _resolverTypeName = _propertyName.camelCaseUpper ~ "Resolver";
+    enum _resolverName = "_" ~ _propertyName ~ "Resolver";
+	
+	return format(`
+        private alias %6$s = GraphBelongsToResolver!(%1$s, %2$s, "%4$s", "%5$s");
+        private %6$s %7$s; 
+        %6$s %3$s(string file = __FILE__, typeof(__LINE__) line = __LINE__) {
+			enforce(graph, "Attempted to use GraphBelongsTo property '%3$s(%2$s)' on model '%1$s' without a graph instance %%s(%%s)", file, line);
+            if (!%7$s) %7$s = new %6$s(this);
+            return %7$s; 
+        }
+	`, P.stringof, M.stringof, _propertyName, _key, foreignKey, _resolverTypeName, _resolverName);
+}
+
+mixin template GraphBelongsTo(P : GraphModelInterface, M : GraphModelInterface, string propertyName = "", string key = "", string foreignKey = "_id") {
+	mixin(defineGraphBelongsToProperty!(P, M, propertyName, key, foreignKey));
+}
+
+/*
 string defineGraphBelongsToProperty(M, string propertyName, string key, string foreignKey)() {
 	import std.string;
 	
@@ -54,4 +87,53 @@ string defineGraphOuterBelongsTo(L, M, string propertyName, string key, string f
 
 mixin template GraphBelongsTo(L, M, string propertyName = "", string key = "", string foreignKey = "_id") {
 	mixin(defineGraphOuterBelongsTo!(L, M, propertyName, key, foreignKey));
+}
+
+*/
+
+version (unittest) {
+	class GraphTestUser : GraphModelInterface {
+		mixin GraphModelImplementation;
+		
+		string id;
+		string name;
+        string bestFriendId;
+
+		override @property string graphId() const { return id; }
+		override @property void graphId(string newId) { id = newId; }
+        
+        mixin GraphBelongsTo!(GraphTestUser, GraphTestUser, "bestFriend", "bestFriendId", "id");        
+	}
+	
+	unittest {
+        auto graph = new Graph;
+        
+		auto david = graph.inject(new GraphTestUser);
+        david.id = "0";
+		david.name = "David";
+
+		auto mia = graph.inject(new GraphTestUser);
+        mia.id = "1";
+		mia.name = "Mia";
+		mia.bestFriendId = "0";
+
+        assert(!mia.bestFriend.resolved);
+        assert(mia.bestFriend.name == "David");
+        assert(mia.bestFriend.resolved);
+	}
+	
+	unittest {
+        auto graph = new Graph;
+        
+		auto david = graph.inject(new GraphTestUser);
+        david.id = "0";
+		david.name = "David";
+
+		auto mia = graph.inject(new GraphTestUser);
+        mia.id = "1";
+		mia.name = "Mia";
+
+        mia.bestFriend.value = david;
+        assert(mia.bestFriendId == "0");
+	}
 }
--- components/graph/source/aura/graph/resolve.d
+++ components/graph/source/aura/graph/resolve.d
@@ -10,16 +10,10 @@ interface GraphResolverInterface {
 
 class GraphResolver(T) : GraphResolverInterface {
     alias ResolverDelegate = T delegate(); 
-	this() {
-	}
-    
+
     this(ResolverDelegate resolver) {
         _resolver = resolver;
     }
-
-	this(T value) {
-		_value = value;
-	}
 
 	bool resolved() const { return _value ? true : false; }
 	void resolve(bool force = false) {
@@ -42,29 +36,81 @@ private:
     ResolverDelegate _resolver;
 }
 
+class GraphBelongsToResolver(P : GraphModelInterface, M : GraphModelInterface, string key, string foreignKey) : GraphResolverInterface {
+    this(P model) {
+        _parent = model;
+    }
+
+	bool resolved() const { return _model ? true : false; }
+	void resolve(bool force = false) {
+        auto lookupValue = __traits(getMember, _parent, key);
+        if (!resolved || force) _model = _parent.graph.find!(M, foreignKey)(lookupValue); 
+    }
+	
+	@property M value() {
+        resolve;
+		return _model;
+    }
+    
+	@property void value(M newValue) {
+        _model = newValue;
+        __traits(getMember, _parent, key) = __traits(getMember, _model, foreignKey);
+    }
+	
+	alias value this;
+	
+private:
+    P _parent; // References the parent model that the resolver resides in
+	M _model;
+}
+
 version (unittest) {
 	class GraphTestUser : GraphModelInterface {
 		mixin GraphModelImplementation;
 		
 		string id;
 		string name;
+        string bestFriendId;
 
 		override @property string graphId() const { return id; }
 		override @property void graphId(string newId) { id = newId; }
+        
+        private alias BestFriendResolver  = GraphBelongsToResolver!(GraphTestUser, GraphTestUser, "bestFriendId", "id");
+        private BestFriendResolver _bestFriendResolver; 
+        @ignore @property BestFriendResolver bestFriend() {
+            if (!_bestFriendResolver)  _bestFriendResolver = new BestFriendResolver(this);
+            return _bestFriendResolver; 
+        }
 	}
 	
 	unittest {
-        alias TestResolve = GraphResolver!(GraphTestUser); 
-		auto belongsTo = new TestResolve;
-		assert(!belongsTo.resolved);
+        auto graph = new Graph;
+        
+		auto david = graph.inject(new GraphTestUser);
+        david.id = "0";
+		david.name = "David";
 
-		auto user = new GraphTestUser;
-		user.name = "David";
+		auto mia = graph.inject(new GraphTestUser);
+        mia.id = "1";
+		mia.name = "Mia";
+		mia.bestFriendId = "0";
 
-		belongsTo = user;
-
-		assert(belongsTo.resolved);
-		assert(belongsTo.name == "David");
+        assert(mia.bestFriend);
+        assert(mia.bestFriend.name == "David");
 	}
 	
+	unittest {
+        auto graph = new Graph;
+        
+		auto david = graph.inject(new GraphTestUser);
+        david.id = "0";
+		david.name = "David";
+
+		auto mia = graph.inject(new GraphTestUser);
+        mia.id = "1";
+		mia.name = "Mia";
+
+        mia.bestFriend.value = david;
+        assert(mia.bestFriendId == "0");
+	}
 }
