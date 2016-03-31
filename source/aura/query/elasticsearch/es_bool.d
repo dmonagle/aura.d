8c9fef086ac619fb2dff8501fe95d65073dd0975
David Monagle <david.monagle@intrica.com.au>
2016-03-31 21:16:45 +1100

    Updated EsBool to use a template

--- source/aura/query/elasticsearch/es_bool.d
+++ source/aura/query/elasticsearch/es_bool.d
@@ -1,73 +1,60 @@
 ﻿module aura.query.elasticsearch.es_bool;
 
 import aura.data.json;
+import std.format;
 
-class EsBool : JsonBuilderBase {
+private string defineEsBoolOccurences(string[] occurenceTypes) {
+    string returnString;
+    foreach(occurenceType; occurenceTypes) {
+        returnString ~= format(`EsBool %1$s(T)(T criteria) { return addOccurence!("%1$s")(criteria); }`, occurenceType);
+    }
+    return returnString;
+}
+
+class EsBoolTemplate(string[] occurenceTypes) : JsonBuilderBase {
 	mixin JsonBuilderCore!EsBool;
+    mixin (defineEsBoolOccurences(occurenceTypes));
 	
 	this() {
 		_json = Json.emptyObject;
-		_json.must = Json.emptyArray;
-		_json.must_not = Json.emptyArray;
-		_json.should = Json.emptyArray;
+        foreach(occurenceType; occurenceTypes) {
+            _json[occurenceType] = Json.emptyArray;
+        }
 	}
-	
-	EsBool must(T : Json)(T criteria) {
-		_json.must ~= criteria;
-		return this;
-	}
-	
-	EsBool must(T : string)(T criteria) {
-		return must(criteria.parseJsonString);
-	}
-	
-	EsBool must(T)(T criteria) {
-		return must(serializeToJson(criteria));
-	}
-	
-	EsBool must_not(T : Json)(T criteria) {
-		_json.must_not ~= criteria;
-		return this;
-	}
-	
-	EsBool must_not(T : string)(T criteria) {
-		return must_not(criteria.parseJsonString);
-	}
-
-	EsBool must_not(T)(T criteria) {
-		return must_not(serializeToJson(criteria));
-	}
-	
-	EsBool should(T : Json)(T criteria) {
-		_json.should ~= criteria;
-		return this;
-	}
-	
-	EsBool should(T : string)(T criteria) {
-		return should(criteria.parseJsonString);
-	}
-
-	EsBool should(T)(T criteria) {
-		return should(serializeToJson(criteria));
-	}
-	
+    	
 	@property bool empty() {
-		if (_json.must.length) return false;
-		if (_json.must_not.length) return false;
-		if (_json.should.length) return false;
+        foreach(occurenceType; occurenceTypes) {
+    		if (_json[occurenceType].length) return false;
+        }
 		return true;
 	}
 	
 	override @property Json json() {
 		auto j = Json.emptyObject;
 		
-		if (_json.must.length) j.must = _json.must;
-		if (_json.must_not.length) j.must_not = _json.must_not;
-		if (_json.should.length) j.should = _json.should;
+        foreach(occurenceType; occurenceTypes) {
+	    	if (_json[occurenceType].length) j[occurenceType] = _json[occurenceType];
+        }
 		
 		return j;
 	}
+
+protected:
+	EsBool addOccurence(string occurenceType, T : Json)(T criteria) {
+		_json[occurenceType] ~= criteria;
+		return this;
+	}
+	
+	EsBool addOccurence(string occurenceType, T : string)(T criteria) {
+		return addOccurence!occurenceType(criteria.parseJsonString);
+	}
+	
+	EsBool addOccurence(string occurenceType, T)(T criteria) {
+		return addOccurence!occurenceType(serializeToJson(criteria));
+	}
 }
+
+alias EsBool = EsBoolTemplate!(["must", "filter", "should", "should_not"]);
 
 unittest {
 	import std.stdio;
@@ -77,9 +64,11 @@ unittest {
 		assert(b.empty);
 		b.must(["David": "Ginny"]);
 		b.should(["Tim": "Alison"]);
+		b.filter(["Jo": true]);
+		b.filter(["Mia": false]);
 		//or.add(MongoOr.build.add(["nested": true]));
 	});
 	
-	//writeln(query.toPrettyString.color(fg.green));
+	writeln(query.toPrettyString.color(fg.green));
 }
 
