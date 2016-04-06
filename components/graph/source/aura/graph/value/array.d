d5953f7cabacebe292bb5affc809e230bd6cc628
David Monagle <david.monagle@intrica.com.au>
2016-04-06 11:24:29 +1000

    Added graphArray helper that takes in a standard array

--- components/graph/source/aura/graph/value/array.d
+++ components/graph/source/aura/graph/value/array.d
@@ -2,6 +2,17 @@
 
 import aura.graph.value.value;
 
+/// Returns a GraphValue of type array containing the same elements of the passed aray as GraphValue
+GraphValue graphArray(T)(T[] a) {
+	GraphValue.Array array;
+	foreach(value; a) {
+		array ~= GraphValue(value);
+	}
+
+	return GraphValue(array);
+}
+
+
 /// Returns a GraphValue of type array containing GraphValues of the given args
 GraphValue graphArray(T...)(T args) {
 	GraphValue.Array array;
